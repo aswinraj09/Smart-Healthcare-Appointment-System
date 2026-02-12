4c2efd7e73ea598cdbd47843595fe8e84a9cf700
aswinraj09 <rajaswin226@gmail.com>
2026-02-12 11:53:35 +0530

    Use Render environment variables

--- has_backend/has_backend/settings.py
+++ has_backend/has_backend/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fx^bk0ji3s)9x!eur@e#m127_ya@y+i*bm3zv=laqanth*r_tt'
+SECRET_KEY = os.getenv("SECRET_KEY", 'django-insecure-fx^bk0ji3s)9x!eur@e#m127_ya@y+i*bm3zv=laqanth*r_tt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 
 # Application definition
@@ -65,8 +65,6 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173", 
 ]
-
-ALLOWED_HOSTS = ["*"]
 
 SESSION_COOKIE_SECURE = False  
 
