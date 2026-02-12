833902449121abe50cfbc49e253770bbc255f16f
aswinraj09 <rajaswin226@gmail.com>
2026-02-12 12:26:46 +0530

    Commit after frontend changes

--- has_backend/has_backend/settings.py
+++ has_backend/has_backend/settings.py
@@ -63,7 +63,8 @@ MIDDLEWARE = [
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173", 
+    "http://localhost:5173",
+    "https://your-frontend.vercel.app",
 ]
 
 SESSION_COOKIE_SECURE = False  
